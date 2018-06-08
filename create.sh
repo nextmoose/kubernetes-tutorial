d523b01b981168dafe6999f8e4cd27bff606395a
Emory Merryman <emory.merryman@gmail.com>
2018-06-08 15:48:22 +0000

    make priv

--- create.sh
+++ create.sh
@@ -8,4 +8,5 @@ docker \
     --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
     --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
     --mount type=bind,source=/dev/vboxdrv,destination=/dev/vboxdrv,readonly=true \
+    --privileged \
     kubernetes-tutorial:$(git rev-parse HEAD)
