81b5b873ff5658c6160f094674244602fd29493f
Emory Merryman <emory.merryman@gmail.com>
2018-06-08 16:39:55 +0000

    add vb dev

--- create.sh
+++ create.sh
@@ -8,5 +8,7 @@ docker \
     --mount type=bind,source=/tmp/.X11-unix,destination=/tmp/.X11-unix,readonly=true \
     --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock,readonly=true \
     --mount type=bind,source=/dev/vboxdrv,destination=/dev/vboxdrv,readonly=true \
+    --mount type=bind,source=/dev/vboxdrvu,destination=/dev/vboxdrvu,readonly=true \
+    --mount type=bind,source=/dev/vboxdnetctl,destination=/dev/vboxnetctl,readonly=true \
     --privileged \
     kubernetes-tutorial:$(git rev-parse HEAD)
